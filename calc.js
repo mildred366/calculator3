fe6df6a6130efea591811a70c158dff98afa2dfd
Mildred Erubore <87650370+mildred366@users.noreply.github.com>
2022-05-28 22:55:52 +0100

    first commit

---
+++ calc.js
@@ -0,0 +1,27 @@
+// let output = document.getElementById("display");
+
+// let buttons = Array.from(document.getElementsByTagName('button'));
+// console.log(buttons);
+
+// buttons.map( buttons =>{
+//     buttons.addEventListener('click', (e) =>{
+//         switch(e.target.innerText){
+//             case 'del':
+//                 display.innerText = '';
+//                 break;
+//             case '=':
+//                 display.innerText = eval(display.innerText);
+//                 break;
+//             case '=':
+//                 try{
+//                     display.innerText = eval(display.innerText);
+//                 }
+//                 catch{
+//                     display.innerText = 'err';
+//                 }
+//                 break;
+//             default:
+//                 display.innerText += e.target.innerText;
+//         }
+//     })
+// } )
