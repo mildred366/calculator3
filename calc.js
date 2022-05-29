761a1395ca5096d2ece7e1594b6a36ee025de80e
Mildred Erubore <87650370+mildred366@users.noreply.github.com>
2022-05-29 23:27:17 +0100

    after adding the equal

--- calc.js
+++ calc.js
@@ -12,34 +12,15 @@ let outputScreen = document.getElementById("output-screen");
             alert("Invalid Expression")
         }
     }
-    function Clear(){
+    function del(){
         outputScreen.value = "";
     }
-    function del(){
-        outputScreen.value = outputScreen.value.slice(4, 0);
-    }
-// let buttons = Array.from(document.getElementsByTagName('button'));
-// console.log(buttons);
 
-// buttons.map( buttons =>{
-//     buttons.addEventListener('click', (e) =>{
-//         switch(e.target.innerText){
-//             case 'del':
-//                 display.innerText = '';
-//                 break;
-//             case '=':
-//                 display.innerText = eval(display.innerText);
-//                 break;
-//             case '=':
-//                 try{
-//                     display.innerText = eval(display.innerText);
-//                 }
-//                 catch{
-//                     display.innerText = 'err';
-//                 }
-//                 break;
-//             default:
-//                 display.innerText += e.target.innerText;
-//         }
-//     })
-// } )
+
+    // function Clear(){
+    //     outputScreen.value = "";
+    // }
+
+    // function del(){
+    //     outputScreen.value = outputScreen.value.slice(0, -2);
+    // }
