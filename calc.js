45afc21264adcf1c481114849cc11f545be60bb2
Mildred Erubore <87650370+mildred366@users.noreply.github.com>
2022-05-29 22:53:24 +0100

    second commit

--- calc.js
+++ calc.js
@@ -1,5 +1,23 @@
-// let output = document.getElementById("display");
+let outputScreen = document.getElementById("output-screen");
 
+    function display(num){
+        outputScreen.value += num;
+    }
+    function Calculate(){
+        try{
+            outputScreen.value += eval(outputScreen.value);
+        }
+        catch(err)
+        {
+            alert("Invalid Expression")
+        }
+    }
+    function Clear(){
+        outputScreen.value = "";
+    }
+    function del(){
+        outputScreen.value = outputScreen.value.slice(4, 0);
+    }
 // let buttons = Array.from(document.getElementsByTagName('button'));
 // console.log(buttons);
 
